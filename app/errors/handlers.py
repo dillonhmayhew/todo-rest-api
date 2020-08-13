05cf105e210331cdc634a2875f681e6f8781e936
dillon <dillonhmayhew@gmail.com>
2020-08-13 11:11:13 -0600

    added error handler 405

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -8,3 +8,7 @@ def not_found(error):
 @bp.app_errorhandler(400)
 def bad_request(error):
     return make_response(jsonify(error='Bad Request'), 400)
+
+@bp.app_errorhandler(405)
+def method_not_allowed(error):
+    return make_response(jsonify(error='Method Not Allowed'), 405)

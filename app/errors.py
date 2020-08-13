0656aa6d21a5b8aa59ab2d6ef20dbc4a80aea28d
dillon <dillonhmayhew@gmail.com>
2020-08-13 08:17:13 -0600

    added 400 errorhandler

--- app/errors.py
+++ app/errors.py
@@ -4,3 +4,7 @@ from app import app
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify(error='Not found'), 404)
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify(error='Bad Request'), 400)
